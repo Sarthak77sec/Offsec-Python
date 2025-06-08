f27dc83ffb1fcbd4a69e8f72e4c6a02e568912d9
Sarthak_77 <sarthak@kali.kali>
2025-06-08 16:01:04 +0530

    tcp client and tcp handler made with python

---
+++ tcp_client.py
@@ -0,0 +1,11 @@
+import socket
+
+server_ip = "127.0.0.1"  # Replace with the server's IP if not local
+server_port = 9998
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, server_port))
+client.send(b"Hello, Server!")
+response = client.recv(4096)
+print(response.decode())
+client.close()
