9983a4dd500b5024aefe2a5ad4b606eb622c6d7d
Sarthak_77 <sarthak@kali.kali>
2026-08-16 13:12:29 +0530

    no changes

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
 server_ip = "127.0.0.1"  # Replace with the server's IP if not local
-server_port = 9998
+server_port = 444
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server_ip, server_port))
