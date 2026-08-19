a29be548c1bcac6700315b7a60a844d14f9e3b04
Sarthak_77 <sarthak@kali.kali>
2026-08-19 22:55:38 +0530

    added recieve function in the file

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -17,4 +17,10 @@ def hexdump(src,length=16,show=True):
         printable=word.translate(HEX_FILTER)
 
         hexa=' '.join([f'{ord(c)}:02X' for c in word])
+        hexwidth=length*3
         results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+        if show:
+            for line in results:
+                print(line)
+            else:
+                return results
